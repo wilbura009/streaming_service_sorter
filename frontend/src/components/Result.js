eea703854d0d2b3347d75c06dd25bdb925e9b355
wlumumba <wyclifflumumba@gmail.com>
2022-04-11 17:35:04 -0500

    css changes

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -7,23 +7,22 @@ import TitlesDataService from "../services/titles"
 import { useState, useEffect } from "react"
 import titles from "../services/titles"
 
-
 //Create separate css and override
 //Main container wont take a background color?
 
-export default function Result( props ) {
-    const params = useParams();
-    const test = useLocation();
-    const imdbID = params.imdbID;
-    console.log(imdbID);
+export default function Result(props) {
+	const params = useParams()
+	const test = useLocation()
+	const imdbID = params.imdbID
+	console.log(imdbID)
 
-    const [title, setTitle] = useState(false);
+	const [title, setTitle] = useState(false)
 
-    useEffect(() => {
+	useEffect(() => {
 		retrieveTitles()
 	}, [])
 
-    //NEED CALL TO DB GIVEN IMBD
+	//NEED CALL TO DB GIVEN IMBD
 	const retrieveTitles = () => {
 		TitlesDataService.getAll()
 			.then(response => {
@@ -36,33 +35,50 @@ export default function Result( props ) {
 			})
 	}
 
-    console.log(title);
+	console.log(title)
 
 	return (
 		<>
-			<Navbar />
-			<Container className={"main-container"}>
-				<Row>
-					<Col sm={5} className="">
-						<Card>
-							<Card.Img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
-							<Card.Body>
-								<Card.Title className="">Card title</Card.Title>
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col>
-						<h1 class="pad">{title && (title[0].title)}</h1>
-						<h5 class="">Watch now</h5>
-						<Card className="bg-warning">
-							<Card.Body className={""}>
-								<Card.Text>Cute Logos that link to provider</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col sm={1} className="border-left"></Col>
-				</Row>
-			</Container>
+			<div className="outer">
+				<Navbar />
+				<Container className={"main-container"}>
+					<Row>
+						<Col sm={5} className="">
+							<Card>
+								<Card.Img src={title && title[0].image} />
+								<Card.Body>
+									<Card.Title className="">Card title</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col>
+							<h1 class="pad">{title && title[0].title}</h1>
+
+							<div class="pad">
+								<h5 class="" style={{ color: "#FFA500" }}>
+									Watch now
+								</h5>
+								<Card className="border border-warning">
+									<Card.Body
+										className=""
+										style={{ backgroundColor: "#2e3840" }}
+									>
+										<Card.Text>Cute Logos that link to provider</Card.Text>
+									</Card.Body>
+								</Card>
+							</div>
+
+							<div>
+								<h5 class="" style={{ color: "#FFA500" }}>
+									Synopsis
+								</h5>
+                                <h7>{title && title[0].description}</h7>
+							</div>
+						</Col>
+						<Col sm={1} className="border-left"></Col>
+					</Row>
+				</Container>
+			</div>
 		</>
 	)
 }
