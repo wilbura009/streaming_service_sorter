946c7a51c029d6cb215ef51966d4e13d41ac7163
wlumumba <wyclifflumumba@gmail.com>
2022-04-13 20:18:13 -0500

    fixed navbar

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default axios.create({
     //URL of the backend server
-    baseURL: "http://localhost:5001/api/v1/home",
+    baseURL: "https://us-east-1.aws.data.mongodb-api.com/app/streamez-sydux/endpoint/",
     headers: {
         "Content-Type": "application/json",
     }
